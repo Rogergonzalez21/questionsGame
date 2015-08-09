fad172790e4562c24164af4d1c8f5fa5ac204896
Roger González <rogergonzalez21@gmail.com>
2015-08-09 16:28:43 -0430

    Added some models

--- questions/models.py
+++ questions/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    question = models.CharField(max_length=128)
+    value = models.ForeignKey(values)
+
+    def __unicode__(self):
+        return self.name
+
+class Values(models.Model):
+	value = models.CharField(max_length=64)
+	points = models.IntegerField(default=0)
+
+	def __unicode__(self):
+		return self.value
